e47525d531640476be571bdaacf0e602b150fafc
Itsm0nka <bahashvaha@gmail.com>
2025-08-29 22:36:59 +0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: { exclude: ['lucide-react'] },
+  base: '/', // важно для правильной работы SPA на проде
   server: {
     proxy: {
-      '/api': { target: 'http://localhost:3001', changeOrigin: true },
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
     },
   },
 });
-
